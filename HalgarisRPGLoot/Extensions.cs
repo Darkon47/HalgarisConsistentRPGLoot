e13947a7a6f0b9d1ac3aad917e25477675ff30fc
Luca <EzioTheDeadPoet@users.noreply.github.com>
2021-06-16 19:05:29 +0200

    index out of bounds fix

--- HalgarisRPGLoot/Extensions.cs
+++ HalgarisRPGLoot/Extensions.cs
@@ -13,6 +13,7 @@ namespace HalgarisRPGLoot
 
         public static T RandomItem<T>(this T[] itms)
         {
+            //return itms[Random.Next(0, (itms.Length-1 <= 0 )? 1 : itms.Length-1)];
             return itms[Random.Next(0, itms.Length)];
         }
 
